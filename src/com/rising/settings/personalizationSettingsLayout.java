a6b4aa33566d4350c554273ec9416a2713321845
LurdRaiden <everethduncan@gmail.com>
2023-09-17 19:19:47 -0400

    Introduce header logo

--- src/com/rising/settings/personalizationSettingsLayout.java
+++ src/com/rising/settings/personalizationSettingsLayout.java
@@ -19,20 +19,38 @@
  import android.os.Bundle;
  import android.content.Context;
  import com.android.settings.R;
+ import android.view.View;
  import com.android.settings.SettingsPreferenceFragment;
  import com.android.internal.logging.nano.MetricsProto;
- 
+ import com.google.android.material.appbar.CollapsingToolbarLayout;
+
  public class personalizationSettingsLayout extends SettingsPreferenceFragment {
  
+    protected CollapsingToolbarLayout mCollapsingToolbarLayout;
+
      @Override
      public void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
- 
+         hideToolbar();
          addPreferencesFromResource(R.xml.rising_dashboard);
      }
  
+     public void onResume() {
+        super.onResume();
+        hideToolbar();
+    }
+
      @Override
      public int getMetricsCategory() {
          return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
      }
+
+    private void hideToolbar() {
+        if (mCollapsingToolbarLayout == null) {
+            mCollapsingToolbarLayout = getActivity().findViewById(R.id.collapsing_toolbar);
+        }
+        if (mCollapsingToolbarLayout != null) {
+            mCollapsingToolbarLayout.setVisibility(View.GONE);
+        }
+    }
  } 
